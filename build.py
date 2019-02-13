18e02725fab552244fb9978467726fee46a94df8
stevezlin <stevezlin@gmail.com>
2019-02-13 13:39:32 -0800

    Final combined HTML using python scripts

--- build.py
+++ build.py
@@ -14,3 +14,18 @@ index_html = top_html + index_html + bottom_html
 open('docs/index.html', 'w+').write(index_html)
 
 
+# Read project HTML file
+projects_html = open ('content/projects.html').read()
+
+# Combine project HTML with top and bottom html templates
+projects_html = top_html + projects_html + bottom_html
+open('docs/projects.html', 'w+').write(projects_html)
+
+# Read contact HTML file 
+contact_html = open ('content/contact.html').read()
+
+# Combine contact_html with top and bottom
+contact_html = top_html + contact_html + bottom_html
+open('docs/contact.html', 'w+').write(contact_html)
+
+print ('Finished combining HTML templates with conent')
